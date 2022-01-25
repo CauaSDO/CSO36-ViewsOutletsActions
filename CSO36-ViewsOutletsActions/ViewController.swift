5a10dd11be3e9474104c6b6ca7953beacc4e432c
CauaSDO <cauasilva@icloud.com>
2022-01-25 11:36:44 -0300

    ViewsOutletsActions

--- CSO36-ViewsOutletsActions/ViewController.swift
+++ CSO36-ViewsOutletsActions/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myView: UIView!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet var buttons: [UIButton]!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func runSomething(_ sender: UIButton) {
+    }
+    
+    @IBAction func change(_ sender: Any) {
+        print("change")
+    }
+    
+    
 }
+
 
